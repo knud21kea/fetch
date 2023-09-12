00fc3b7d9886262053bd074eb06707fe9f3310a2
knud21kea <grah0070@stud.kea.dk>
2023-09-12 22:08:53 +0200

    route removed

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-import fetch from 'node-fetch'; // just to try a server-side fetch in Node.js
 import express from 'express';
 import path from "path";
 
 const app = express();
 app.use(express.static("public"));
 
-fetch(`https://the-trivia-api.com/api/questions?limit=10`)
-    .then((response) => response.json())
-    .then((result) => {
-
-        // API
-
-        app.get("/", (req, res) => {
-            res.sendFile(path.resolve("public/quiz/html/quiz.html"))
-        });
-
-        app.get("/quiz/:id", (req, res) => {
-            const quizToGet = result[req.params.id];
-            res.send(quizToGet);
-        })
-    })
-    .catch((error) => {
-        console.error("Error fetching trivia data:", error);
-    });
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/quiz/html/quiz.html"))
+});
 
 const PORT = 8080;
 app.listen(PORT, (error) => {
